be07ecd72b517470430de0ab48cf5658a95374d5
j_eun0415 <j_eun0415@naver.com>
2024-07-22 17:56:58 +0900

    24.07.22

---
+++ java_work/ex18/src/main/java/org/example/LPrint.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class LPrint implements Printable{
+
+    @Override
+    public void print(String doc) {
+        System.out.println("LG프린터");
+        System.out.println(doc);
+    }
+
+    @Override
+    public void doA() {
+
+    }
+}
